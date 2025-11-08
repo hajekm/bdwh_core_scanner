c719d3661b15dca843c09ae509e60ae25cef18f7
Milan Hajek <milan.hajek89@gmail.com>
2025-11-08 10:59:55 +0100

    valid loction format

--- utils/location_format.go
+++ utils/location_format.go
@@ -3,6 +3,6 @@ package utils
 import "regexp"
 
 func IsValidLocationFormat(s string) bool {
-	re := regexp.MustCompile(`^[A-Za-z0-9]+-[A-Za-z0-9]+-(?:[0-9]|[1-9][0-9])-[A-Za-z0-9]+$`)
+	re := regexp.MustCompile(`^[A-Za-z0-9]+(?:-[A-Za-z0-9]+)?-(?:[0-9]|[1-9][0-9])-[A-Za-z0-9]+$`)
 	return re.MatchString(s)
 }
